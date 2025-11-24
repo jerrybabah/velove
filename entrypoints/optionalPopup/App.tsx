e7061e1e7ccd2cf5e2e2fb6c1c8a25d1885c8409
jerrybabah <ghdtjs0417@gmail.com>
2025-11-24 16:40:18 +0900

    feat: update title and button text for improved clarity in optional popup

--- entrypoints/optionalPopup/App.tsx
+++ entrypoints/optionalPopup/App.tsx
@@ -11,10 +11,17 @@ export function App() {
     <ConfigProvider theme={theme}>
       <Result
         status='404'
-        title='velog 전용 익스텐션입니다.'
+        title='velog.io 전용 익스텐션입니다'
+        subTitle='velog에서 클릭하면 사이드패널이 열립니다'
         extra={
-          <Button type='primary' href='https://velog.io' target='_blank'>
-            velog 열기
+          <Button
+            type='primary'
+            href='https://velog.io'
+            target='_blank'
+            block
+            size='large'
+          >
+            velog.io 새탭 열기
           </Button>
         }
       />
